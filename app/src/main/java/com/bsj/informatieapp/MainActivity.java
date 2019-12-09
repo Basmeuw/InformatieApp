1d16648d610a1ffc51c34a8ea0de7c04b7eae993
Basmeuw <32522618+Basmeuw@users.noreply.github.com>
2019-12-09 15:46:18 +0100

    Bottom Navigation Drawer toegevoegd

--- app/src/main/java/com/bsj/informatieapp/MainActivity.java
+++ app/src/main/java/com/bsj/informatieapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.bsj.informatieapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        bottomNav.setSelectedItemId(R.id.nav_start);
+
+        // Open the start page
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).addToBackStack(null).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_start:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_nieuws:
+                            selectedFragment = new NewsFragment();
+                            break;
+                        case R.id.nav_events:
+                            selectedFragment = new EventsFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).addToBackStack(null).commit();
+                    return true;
+                }
+            };
 }
---
+++ app/src/main/java/com/bsj/informatieapp/NewsFragment.java
@@ -0,0 +1,17 @@
+package com.bsj.informatieapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+}
