1ee2e4216a707c2693853ea3fb60d3087a941069
Basmeuw <basmeuwissen1@gmail.com>
2020-02-19 14:33:43 +0100

    News op home page gefixt

--- app/src/main/java/com/bsj/informatieapp/HomeFragment.java
+++ app/src/main/java/com/bsj/informatieapp/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.bsj.informatieapp;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
@@ -7,20 +8,25 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.bsj.informatieapp.events.EventsFragment;
+import com.bsj.informatieapp.news.News;
 import com.bsj.informatieapp.news.NewsFragment;
 import com.bsj.informatieapp.news.NewsManager;
 import com.bsj.informatieapp.news.NewsViewModel;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment implements View.OnClickListener{
 
@@ -34,15 +40,29 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         View v = inflater.inflate(R.layout.fragment_home, container, false);
         fragmentManager = getActivity().getSupportFragmentManager();
 
-        newsViewModel = ViewModelProviders.of(this).get(NewsViewModel.class);
-        // News handling
-
-        View newsItem = v.findViewById(R.id.newsItem1);
-        TextView newsText = newsItem.findViewById(R.id.adapter_title);
-        TextView newsSource = newsItem.findViewById(R.id.adapter_source);
+        final View newsItem = v.findViewById(R.id.newsItem1);
+        final TextView newsText = newsItem.findViewById(R.id.adapter_title);
+        final TextView newsSource = newsItem.findViewById(R.id.adapter_source);
         ImageView newsImage = newsItem.findViewById(R.id.adapter_thumbnail);
         Button newsReadMore = v.findViewById(R.id.home_newsReadMore);
         newsReadMore.setOnClickListener(this);
+
+        newsViewModel = ViewModelProviders.of(this).get(NewsViewModel.class);
+
+        newsViewModel.getAllNewsArticles().observe(getViewLifecycleOwner(), new Observer<ArrayList<News>>() {
+            @Override
+            public void onChanged(@Nullable ArrayList<News> newsArticles) {
+                News mostImportant = newsArticles.get(0);
+                newsText.setText(mostImportant.getTitle());
+                newsSource.setText(mostImportant.getSource());
+//                newsItem.setOnClickListener(this);
+            }
+        });
+
+
+        // News handling
+
+
 
 //        newsViewModel.getAllNewsArticles().observe();
 
@@ -56,6 +76,9 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
             case R.id.home_newsReadMore:{
                 // Load news page
                 fragmentManager.beginTransaction().replace(R.id.fragment_container, new NewsFragment()).addToBackStack(null).commit();
+            }
+            case R.id.newsItem1:{
+
             }
 //            case R.id.home_eventsReadMore:{
 //                // Load events page
