90bb2719f980dc3db9c34770bf095a03f1cfcfc7
Basmeuw <basmeuwissen1@gmail.com>
2020-03-02 10:41:38 +0100

    EventViewModel gemaakt

--- app/src/main/java/com/bsj/informatieapp/events/Event.java
+++ app/src/main/java/com/bsj/informatieapp/events/Event.java
@@ -3,64 +3,70 @@ package com.bsj.informatieapp.events;
 import java.util.Date;
 
 public class Event {
-    private Date date;
-    private String name;
-    private float lat;
-    private float lon;
-    private float startTime;
-    private float endTime;
+    public String id;
+    public String category;
+    public String startTime;
+    public String endTime;
+    public String name;
+    public String owner;
+    public String place;
+    public String type;
+    public String description;
+    public int attendingCount;
+    public String informationSource;
+    public String date;
 
-    public Event(Date date, String name, float lat, float lon, float startTime, float endTime) {
-        this.date = date;
-        this.name = name;
-        this.lat = lat;
-        this.lon = lon;
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setLocation(float lat, float lon){
-        this.lat = lat;
-        this.lon = lon;
-    }
-
-    public float getLat(){
-        return lat;
-    }
-
-    public float getLon(){
-        return lon;
-    }
-
-    public float getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(float startTime) {
-        this.startTime = startTime;
-    }
-
-    public float getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(float endTime) {
-        this.endTime = endTime;
-    }
+//    public Event(Date date, String name, float lat, float lon, float startTime, float endTime) {
+//        this.date = date;
+//        this.name = name;
+//        this.lat = lat;
+//        this.lon = lon;
+//        this.startTime = startTime;
+//        this.endTime = endTime;
+//    }
+//
+//    public Date getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(Date date) {
+//        this.date = date;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setLocation(float lat, float lon){
+//        this.lat = lat;
+//        this.lon = lon;
+//    }
+//
+//    public float getLat(){
+//        return lat;
+//    }
+//
+//    public float getLon(){
+//        return lon;
+//    }
+//
+//    public float getStartTime() {
+//        return startTime;
+//    }
+//
+//    public void setStartTime(float startTime) {
+//        this.startTime = startTime;
+//    }
+//
+//    public float getEndTime() {
+//        return endTime;
+//    }
+//
+//    public void setEndTime(float endTime) {
+//        this.endTime = endTime;
+//    }
 }
---
+++ app/src/main/java/com/bsj/informatieapp/events/EventViewModel.java
@@ -0,0 +1,62 @@
+package com.bsj.informatieapp.events;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.bsj.informatieapp.news.News;
+
+import org.json.JSONArray;
+
+public class EventViewModel {
+
+    private MutableLiveData<Event[]> events;
+
+    public LiveData<Event[]> getAllEvents(Context context) {
+        checkIfNewsLoaded(context);
+        return events;
+    }
+
+    private void checkIfNewsLoaded(Context context) {
+        if (events == null) {
+            events = new MutableLiveData<>();
+            loadNews(context);
+        }
+    }
+
+    private void loadNews(Context context) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url = "http://schondeln.eitilop-bali.nl/get.php?category[0]=Event";
+
+        JsonArrayRequest stringRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        Gson gson = new Gson();
+                        Log.e("customdebug", response.toString());
+                        Event[] event = gson.fromJson(response.toString(), Event[].class);
+                        events.postValue(event);
+                        //Log.e("customdebug", newsArticles.getValue()[0].title);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Log.e("customdebug", "Mission failed! We'll get em next time.");
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+
+}
